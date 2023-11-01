4a8d1427efb767194333e12decf15429de509eb8
dandorock <daniel-rl-98@hotmail.com>
2023-11-01 16:53:27 -0300

    alteracao

--- src/Controllers/authenticate.js
+++ src/Controllers/authenticate.js
@@ -1,32 +1,25 @@
 
 class Api {
 
-    bearerToken;
-
-    headers = {
-        token: process.env.TOKEN,
-        appkey: process.env.APPKEY,
-        username: process.env.USER,
-        password: process.env.PASS
-    }
-   
-    constructor(req, res) {
-        console.log(this.headers )
-        this.authenticate(req, res)
-    }
+    bearerToken
 
     async authenticate(req, res) {
 
         const response = []
         const url = 'https://api.sankhya.com.br/login'
-        const headers =  this.headers
+
 
         const result = await fetch(url, {
             method: "POST",
             mode: "cors",
             cache: "no-cache",
             credentials: "same-origin",
-            headers,
+            headers: {
+                token: process.env.TOKEN,
+                appkey: process.env.APPKEY,
+                username: process.env.USER,
+                password: process.env.PASS
+            },
             redirect: "follow",
             referrerPolicy: "no-referrer",
             body: JSON.stringify({}),
--- src/Controllers/index.js
+++ src/Controllers/index.js
@@ -5,8 +5,8 @@ module.exports = {
 
          requestApi(req,res){
 
-            const api = new Api(req,res)
-
+            const api = new Api()
+            api.authenticate(req,res)
 
            /* const t = req.query.t
             console.log(t)*/
